80979e5b2017e33e253761dd9c148736373db535
Timothy Concepcion <timothyjtc@outlook.com>
2024-10-20 18:07:50 +0800

    added pitches.h, revised swe

--- swe.bat
+++ swe.bat
@@ -1,22 +1,17 @@
 @echo off
 
+if exist "%~dp0\src\main.cpp" del "%~dp0\src\main.cpp"
+
 if "%1" == "" (
     echo Usage: swe ^<folder^> ^<file^>
-    goto :exit
+    goto exit
 )
 
 if "%2" == "" (
     echo Usage: swe ^<folder^> ^<file^>
-    goto :exit
+    goto exit
 )
 
-if exist "%~dp0\src\main.cpp" (
-    del "%~dp0\src\main.cpp"
-) else (
-    goto :skip
-)
-
-:skip
 mklink /H "%~dp0\src\main.cpp" "%~dp0\src_swe\%1\%2.cpp"
 
 :exit
