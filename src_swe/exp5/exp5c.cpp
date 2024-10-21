81c23dde664259b6e9b4c95737f3c82a59cb1a7a
Timothy James Concepcion <timothyjtc@outlook.com>
2024-10-22 06:28:02 +0800

    swapped digital buttons for exp5c

--- src_swe/exp5/exp5c.cpp
+++ src_swe/exp5/exp5c.cpp
@@ -1,18 +1,19 @@
 #include <Arduino.h>
 #include "pitches.h"
 
-const int threshold = 10;
 const int speaker = 9;
 int notes[] = {NOTE_A1, NOTE_E5, NOTE_FS7};
 
 void setup() {
-    pinMode(speaker,OUTPUT);
+    pinMode(speaker, OUTPUT);
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
 }
 
 void loop() {
-    for (int thisSensor = 0; thisSensor < 3; thisSensor++) {
-        int sensorReading = analogRead(thisSensor);
-        if (sensorReading > threshold) {
+    for (int thisSensor = 2; thisSensor < 5; thisSensor++) {
+        if (digitalRead(thisSensor)) {
             tone(speaker, notes[thisSensor]);
         } 
         noTone(speaker);
